b565e3093b41ebcdb134ba19e04c70b1c6d489a5
oberry <cev2108@innovaccion.mx>
2023-01-17 23:21:48 -0600

    Este es el primer commit

---
+++ usuario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Datos Usuario</title>
+</head>
+<body>
+<?php
+$usuario= $_POST['usuario'];
+$password= $_POST['password'];
+
+
+?>
+</body>
+</html>
